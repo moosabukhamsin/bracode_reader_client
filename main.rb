655a55962bc321cfc3378f1187e1be42ce5a8bb9
moosabukhamsin <moosabukhamsin@gmail.com>
2014-12-22 21:25:15 +0300

    foundation of barcode clientside

--- main.rb
+++ main.rb
@@ -1,9 +1,37 @@
-class barcode
-	var barcode_numbr
-	def getbarcode_numbr()
-		barcode_numbr=gets.chomp
-	end
+require 'sequel'
+require "net/http"
+require "uri"
+require 'json'
 
-	sequel
+class Barcode
+  attr_accessor :db
 
+  def db
+    if @db.nil?
+      @db=Sequel.connect('mysql://root:123456@localhost/barcode')
+    end
+    return @db
+  end
+
+  def install
+    self.db().create_table :items do
+      primary_key :id
+      String :name
+      String :number
+    end
+  end
+
+  def update
+    uri = URI.parse("http://localhost:3000/items.json")
+    response = Net::HTTP.get_response(uri)
+    my_hash = JSON.parse(response.body)
+    items = self.db()[:items]
+    my_hash.each do |mh|
+      items.insert({name: mh[:name], number: mh[:number]})
+    end
+  end
+
+  def getname(number)
+    return self.db[:items].where(number: number).first[:name]
+  end
 end
